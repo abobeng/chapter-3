9c6575c4811c8c7b8932a5eefac1354a019b3a3d
abobeng <91883862+abobeng@users.noreply.github.com>
2021-10-27 11:19:39 +0000

    Add files via upload

---
+++ ques13.2.py
@@ -0,0 +1,5 @@
+x = float(input(' enter a number:'))
+ from math import sin, cos, tan
+print ('sin(x)=', sin(x))
+print ('cosine(x)=', cos(x))
+print ('tangent(x)=', tan(x))
---
+++ ques14.2.py
@@ -0,0 +1,3 @@
+x = int(input('enter angle in degrees:'))
+from math import sin
+print (sin(x))
